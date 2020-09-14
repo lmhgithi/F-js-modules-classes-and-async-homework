e7c3f07979bb387bbdb25daec23b35c161b8afa4
lmhgithi <lmhfox@foxmail.com>
2020-09-14 19:38:08 +0800

    [lmh] finsih all homework

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,9 @@
+import getUrl from './utils'
+import * as constants from './constant'
+const url = getUrl(constants.ADDRESS, constants.PORT, constants.PATH)
+const region = constants.REGION;
 
+export {
+    url,
+    region
+}
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,30 @@
-export const getPoetry
+// export const getPoetry = () => {
+//     const URL='https://v1.jinrishici.com/all.json'
+//     return fetch(URL, {
+//         method: 'GET',
+//     }).then(response => {
+//         if(response.status == 200){
+//             return Promise.resolve(response.json())
+//         } else {
+//             return Promise.reject('sth wrong')
+//         }
+//     }).then(data => {
+//         return [data.origin, data.author, data.content]
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+// }
+
+export const getPoetry = async () => {
+    try{
+        const URL='https://v1.jinrishici.com/all.json'
+        const response = await fetch(URL, {method: 'GET',})
+        const jsonData = await response.json()
+        const result = [jsonData.origin, jsonData.author, jsonData.content]
+
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
